fcfac25b52bd0767d9a5985ac0393acfe155de9b
elizauema <eliza.uema@gmail.com>
2021-01-27 19:16:23 -0300

    adicionar mais 2 prateleiras e validaçao do array

--- Treinamento/src/main/java/br/com/eliza/Estante.java
+++ Treinamento/src/main/java/br/com/eliza/Estante.java
@@ -8,14 +8,21 @@ public class Estante {
 	private Prateleira[] prateleiras = new Prateleira[5];
 
 	public Estante(Prateleira... p) {
-		// varargs ... forma de receber uma qtde de argumentos, pode receber varias estantes
-		//System.out.println(Arrays.deepToString(prateleiras));  O p vira um array
-		this.prateleiras = p;
-//    validar para que sempre seja 5
+		// varargs ... forma de receber uma qtde de argumentos, pode receber varias
+		// estantes
+		// System.out.println(Arrays.deepToString(prateleiras)); O p vira um array
+		// validar para que sempre seja 5
+		if (p.length == 5) {
+			this.prateleiras = p;
+		} else {
+			throw new IllegalArgumentException("Qtde prateleira inválido");
+		}
 	}
+
 	public Prateleira[] getPrateleiras() {
 		return this.prateleiras;
 	}
+
 	@Override
 	public String toString() {
 		return "Estante [prateleiras=" + Arrays.toString(prateleiras) + "]";
--- Treinamento/src/main/java/br/com/eliza/Principal.java
+++ Treinamento/src/main/java/br/com/eliza/Principal.java
@@ -39,11 +39,11 @@ public class Principal {
 		// criar prateleira2 com livros diferentes
 		// imprimir os livros da prateleira2
 
-		Livro livro21 = new Livro("titulo21", "autor21", 2021, "editora 21");
-		Livro livro22 = new Livro("titulo22", "autor22", 2022, "editora 22");
-		Livro livro23 = new Livro("titulo23", "autor23", 2023, "editora 23");
-		Livro livro24 = new Livro("titulo24", "autor24", 2024, "editora 24");
-		Livro livro25 = new Livro("titulo25", "autor25", 2025, "editora 25");
+		Livro livro21 = new Livro("titulo21", "autor21", 2002, "editora 21");
+		Livro livro22 = new Livro("titulo22", "autor22", 2002, "editora 22");
+		Livro livro23 = new Livro("titulo23", "autor23", 2003, "editora 23");
+		Livro livro24 = new Livro("titulo24", "autor24", 2004, "editora 24");
+		Livro livro25 = new Livro("titulo25", "autor25", 2005, "editora 25");
 
 		Prateleira prateleira2 = new Prateleira();
 		prateleira2.adicionarLivro(livro21);
@@ -52,11 +52,11 @@ public class Principal {
 		prateleira2.adicionarLivro(livro24);
 		prateleira2.adicionarLivro(livro25);
 
-		Livro livro31 = new Livro("titulo31", "autor31", 2021, "editora 31");
-		Livro livro32 = new Livro("titulo32", "autor32", 2022, "editora 32");
-		Livro livro33 = new Livro("titulo33", "autor33", 2023, "editora 33");
-		Livro livro34 = new Livro("titulo34", "autor34", 2024, "editora 34");
-		Livro livro35 = new Livro("titulo35", "autor35", 2025, "editora 35");
+		Livro livro31 = new Livro("titulo31", "autor31", 2003, "editora 31");
+		Livro livro32 = new Livro("titulo32", "autor32", 2003, "editora 32");
+		Livro livro33 = new Livro("titulo33", "autor33", 2003, "editora 33");
+		Livro livro34 = new Livro("titulo34", "autor34", 2003, "editora 34");
+		Livro livro35 = new Livro("titulo35", "autor35", 2003, "editora 35");
 
 		Prateleira prateleira3 = new Prateleira();
 		prateleira3.adicionarLivro(livro31);
@@ -65,13 +65,40 @@ public class Principal {
 		prateleira3.adicionarLivro(livro34);
 		prateleira3.adicionarLivro(livro35);
 
+		Livro livro41 = new Livro("titulo41", "autor41", 2004, "editora 41");
+		Livro livro42 = new Livro("titulo42", "autor42", 2004, "editora 42");
+		Livro livro43 = new Livro("titulo43", "autor43", 2004, "editora 43");
+		Livro livro44 = new Livro("titulo44", "autor44", 2004, "editora 44");
+		Livro livro45 = new Livro("titulo45", "autor45", 2004, "editora 45");
+
+		Prateleira prateleira4 = new Prateleira();
+		prateleira4.adicionarLivro(livro41);
+		prateleira4.adicionarLivro(livro42);
+		prateleira4.adicionarLivro(livro43);
+		prateleira4.adicionarLivro(livro44);
+		prateleira4.adicionarLivro(livro45);
+
+		Livro livro51 = new Livro("titulo51", "autor51", 2005, "editora 51");
+		Livro livro52 = new Livro("titulo52", "autor52", 2005, "editora 52");
+		Livro livro53 = new Livro("titulo53", "autor53", 2005, "editora 53");
+		Livro livro54 = new Livro("titulo54", "autor54", 2005, "editora 54");
+		Livro livro55 = new Livro("titulo55", "autor55", 2005, "editora 55");
+
+		Prateleira prateleira5 = new Prateleira();
+		prateleira5.adicionarLivro(livro51);
+		prateleira5.adicionarLivro(livro52);
+		prateleira5.adicionarLivro(livro53);
+		prateleira5.adicionarLivro(livro54);
+		prateleira5.adicionarLivro(livro55);
+
+		
 //		int i = 0;
 //		while (i < 5) {
 //			System.out.println(prateleira2.getLivros().get(i).getTitulo());
 //			i++;
 //		}
 
-		Estante estante = new Estante(prateleira1, prateleira2, prateleira3);
+		Estante estante = new Estante(prateleira1, prateleira2, prateleira3,prateleira4,prateleira5);
 
 		for (int i = 0; i < estante.getPrateleiras().length; i++) {
 			System.out.println("Prateleira " + (i + 1));
